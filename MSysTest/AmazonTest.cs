bbaa744164996bafdd58f66739002304f67970b7
vinothraj s <vino1691@gmail.com>
2021-09-13 20:09:50 +0530

    SystemtestforEncora

---
+++ MSysTest/AmazonTest.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace MSysTest
+{
+    [TestClass]
+    public class AmazonTest
+    {
+        public TestContext TestContext { get; set; }
+        protected static IWebDriver WebDriver { get; set; }
+        protected static Exception Exception { get; set; }
+        public static void LaunchBrowser(string AppUrl, string BrowserExePath, string DownloadFilePath)
+        {
+            GetDriver(BrowserExePath, DownloadFilePath);
+            UrlPass(AppUrl);
+            MaximizeWindow();
+            Thread.Sleep(3000);
+        }
+     
+        public static void UrlPass(string AppUrl)
+        {
+            WebDriver.Url = AppUrl;
+        }
+        public static void MaximizeWindow()
+        {
+            WebDriver.Manage().Window.Maximize();
+        }
+        public static void WebdriverClose()
+        {
+            WebDriver.Close();
+        }
+
+
+        protected static IWebDriver GetDriver(string browserpath, string downloadfilepath)
+        {
+            try
+            {
+                var chromeOptions = new ChromeOptions();
+                chromeOptions.AddArguments("--disable-notifications");
+                chromeOptions.AddUserProfilePreference("download.default_directory", downloadfilepath);
+                chromeOptions.AddUserProfilePreference("download.prompt_for_download", false);
+                chromeOptions.AddUserProfilePreference("download.directory_upgrade", true);
+                chromeOptions.AddUserProfilePreference("safebrowsing.enabled", true);
+                chromeOptions.AddUserProfilePreference("profile.default_content_setting_values.automatic_downloads", 1);
+                chromeOptions.Proxy = null;
+                WebDriver = new ChromeDriver(browserpath, chromeOptions);
+                return WebDriver;
+            }
+            catch (Exception ex)
+            {
+                Exception = ex;
+            }
+            return null;
+        }
+
+        public static IEnumerable<object[]> ReadExcel()
+        {
+            //Create Worksheet object
+            using (ExcelPackage package = new ExcelPackage(new FileInfo("Data.xlsx")))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["Sheet1"];
+                int rowcount = worksheet.Dimension.End.Row;
+                for (int i = 2; i <= rowcount; i++)
+                {
+                    yield return new object[]{
+                        worksheet.Cells[i,1].Value?.ToString().Trim(),
+                        worksheet.Cells[i,2].Value?.ToString().Trim(),
+                        worksheet.Cells[i,3].Value?.ToString().Trim()
+                    };
+                    }
+                }
+
+            }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\data.csv", "data#csv", DataAccessMethod.Sequential)]
+        [DynamicData(nameof(ReadExcel), DynamicDataSourceType.Method)]
+        [TestMethod]
+        public void Amazon(string URL, string SearchType, string MobileBrand)
+        {
+            //Launch the browser             
+            LaunchBrowser(URL, POM.PATH_DRIVER, POM.PATH_DOWNLOAD);                                   
+            
+            WebDriver.FindElement(By.XPath(POM.BUTTON_SIGNIN)).Click();                        
+            
+            var Username = WebDriver.FindElement(By.Id(POM.USERNAME));
+            Username.SendKeys(TestContext.DataRow[1].ToString());            
+            WebDriver.FindElement(By.Id(POM.BUTTON_CONTINUE)).Click();
+           
+            var Password = WebDriver.FindElement(By.Id(POM.PASSWORD));
+            Password.SendKeys(TestContext.DataRow[2].ToString());            
+            WebDriver.FindElement(By.Id(POM.BUTTON_SUBMIT)).Click();           
+            
+            //Select class to hadel the List Items
+            SelectElement oSelection = new SelectElement(WebDriver.FindElement(By.Id("searchDropdownBox")));
+
+            IList<IWebElement> oSize = oSelection.Options;
+
+            int iListSize = oSize.Count;
+            // Setting up the loop to print all the options
+            for (int i = 0; i < iListSize; i++)
+            {
+                // Storing the value of the option	
+                String sValue = oSelection.Options.ElementAt(i).Text;
+                // Printing the stored value
+                Console.WriteLine("Value of the Select item is : " + sValue);
+
+                // Putting a check on each option that if any of the option is equal to 'Africa" then select it 
+                //if (sValue.Equals("Electronics"))
+                if (sValue.Equals(SearchType))
+                {
+                    oSelection.SelectByIndex(i);
+                    break;
+                }
+            }
+
+            var SearchInput = WebDriver.FindElement(By.Id(POM.INPUTBOX_SEARCH));
+          //  SearchInput.SendKeys("One Plus Mobile");
+            SearchInput.SendKeys(MobileBrand);
+            WebDriver.FindElement(By.Id(POM.BUTTON_SEARCH)).Click();
+            WebDriver.FindElement(By.XPath(POM.CHECKBOX_BRAND)).Click();
+            WebDriver.FindElement(By.XPath(POM.BUTTON_CHOOSE)).Click();
+
+            var Price = WebDriver.FindElement(By.Id(POM.GET_PRICE)).Text;            
+            Console.WriteLine("Retrived Mobile Price is: ", Price);
+
+            WebDriver.FindElement(By.Id(POM.BUTTON_ADDTOCART)).Click();
+        }
+
+        [TestCleanup]
+        public void TestCleaup()
+        {
+            WebdriverClose();            
+        }
+
+    }
+}
--- MSysTest/POM.cs
+++ MSysTest/POM.cs
@@ -8,19 +8,20 @@ namespace MSysTest
 {
     public class POM
     {
-        public static string URL = "https://www.irctc.co.in/nget/train-search";        
-        public static string PATH_DRIVER = @"C:\Program Files\Driver";        
+        public static string URL = "https://www.amazon.com/";        
+        public static string PATH_DRIVER = @"E:\Driver\";        
         public static string PATH_DOWNLOAD = @"C:\Users\Downloads\";
         public static string BUTTON_COOKIES_ACCEPT = "/html/body/div[5]/div[3]/button";       
-        public static string BUTTON_OK = "//button[text()='OK']";
-        public static string BUTTON_SCHEDULE = "//button[text()='OK']";
-        public static string FROM = "//*[@id='origin']/span/input";
-        public static string TO = "//*[@id='destination']/span/input";
-        public static string BUTTON_SEARCH = "//button[text()='Search']";
-        public static string DATE = "//*[@id='jDate']/span/input";
-        public static string CLASS = "//a[@id='cllink-13237-CC-1']";
-        public static string BOOKNOW = "//a[@id='13237-3A-GN-0']";
-        public static string TrainName = "//*[@id='divMain']/div/app-train-list/div[4]/div/div[5]/div[1]/div[1]/app-train-avl-enq/div[1]/div[1]/div[1]/strong/text()";
-        public static string TrainSchedule = "//span[text()='Train Schedule'][1]";
+        public static string BUTTON_SIGNIN = "//*[@id='nav-link-accountList-nav-line-1']";
+        public static string BUTTON_CONTINUE = "continue";
+        public static string USERNAME = "ap_email";
+        public static string PASSWORD = "ap_password";
+        public static string BUTTON_SUBMIT = "signInSubmit";
+        public static string INPUTBOX_SEARCH = "twotabsearchtextbox";
+        public static string BUTTON_SEARCH = "nav-search-submit-button";
+        public static string CHECKBOX_BRAND = "//span[text()='OnePlus']";
+        public static string BUTTON_CHOOSE = "//h2/a/span[contains(text(),'Model BE2011')]";
+        public static string GET_PRICE = "priceblock_ourprice";
+        public static string BUTTON_ADDTOCART = "add-to-cart-button";
     }
 }
